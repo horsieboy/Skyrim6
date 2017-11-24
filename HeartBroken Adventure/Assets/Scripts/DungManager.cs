8c61a7f75876699cfd7ce418076a1489c398c441
horsieboy <mr.volpe5@gmail.com>
2017-11-25 00:46:04 +0300

    Ruki-bazooki
    +generatsiiya
    -Stabil'nost'

--- HeartBroken Adventure/Assets/Scripts/DungManager.cs
+++ HeartBroken Adventure/Assets/Scripts/DungManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,28 +7,15 @@ public class DungManager : MonoBehaviour {
 
     #region Variables
 
-    public class Count
-    {
-
-        public int MinValue;
-        public int MaxValue;
-
-        public Count(int min, int max)
-        {
-            MinValue = min;
-
-            MaxValue = max;
-        }
-
-    }
-
     enum Direction
     {
         East, South, West, North
     }
 
     [Header("Number of Rooms to Generate")]
-    public int Rooms;
+  	public int Rooms;
+
+	public int GetRooms {get { return Mathf.Abs (Rooms);}}
 
     [Header("Dungeon Parts Parameters")]
     public int StartingXCoord = 0;
@@ -42,187 +30,157 @@ public class DungManager : MonoBehaviour {
 
     //Board Stuff
     private Transform DungHolder;
-    private List<Vector2> gridPositions = new List<Vector2>();
-    private int CurrentRoom = 0;
+	private int currentRoom;
 
     //Corridor Stuff
     private int CorridorX;
     private int CorridorY;
     private Direction direction;
     private Direction incomingDirection;
-    // Even needed?
 
     #endregion
 
-    void InitializeList()
-    {
+	#region VectorDungeon
 
-        gridPositions.Clear();
+	private List<Vector2> GroundBoard = new List<Vector2>();
 
-        for (int i = 1; i < Width; i++)
-        {
-            for (int j = 1; j < Height; j++)
-            {
-                gridPositions.Add(new Vector2(i, j));
-            }
-        }
+	private List<Vector2> WallBoard = new List<Vector2>();
 
-    }
+	void GrandCleaning(){
+		TerminateOvelappingTiles ();
+		CleanUpArrays (GroundBoard);
+		CleanUpArrays (WallBoard);
+	}
 
-    void SpawnObjectAtCoordinates(GameObject[] objects, int x, int y)
+	void CleanUpArrays(List<Vector2> VectorList){
+		for (int i = 0; i < VectorList.Count; i++) {
+			for (int j = 0; j < VectorList.Count; j++) {
+				if (VectorList [i] == VectorList [j] && i != j)
+					VectorList.RemoveAt (i);
+			}
+		}
+	}
+
+	void TerminateOvelappingTiles(){
+	
+		for (int i = 0; i < GroundBoard.Count; i++) {
+			for (int j = 0; j < WallBoard.Count; j++) {
+				if (GroundBoard [i] == WallBoard [j])
+					WallBoard.RemoveAt (j);
+			}
+		}
+	
+	}
+
+	void InitializeTiles(){
+		foreach (var tile in GroundBoard) {
+			SpawnObjectAtCoordinates (FloorTiles, tile);
+		}
+		foreach (var item in WallBoard) {
+			SpawnObjectAtCoordinates (WallTiles, item);
+		}
+	}
+
+	void CreateVectorDungeon()
+	{
+		int VectorXbasis = StartingXCoord;
+
+		int VectorYBasis = StartingYCoord;
+
+		for (int x = 0; x < Width; x++) {
+			for (int y = 0; y < Height; y++) {
+				if (x == 0 || y == 0 || x == Width - 1 || y == Height - 1) {
+					WallBoard.Add (new Vector2 (VectorXbasis + x, VectorYBasis + y));
+				} else {
+					GroundBoard.Add (new Vector2 (VectorXbasis + x, VectorYBasis + y));
+				}
+			}
+		}
+		if (currentRoom == GetRooms)
+			return;
+		CreateVectorTunnel (VectorXbasis, VectorYBasis, Width, Height);
+	}
+				
+	void CreateVectorTunnel(int xBasis, int yBasis, int width, int height){
+	
+		direction = (Direction)Random.Range (0, 4);
+
+		if (direction == incomingDirection)
+			direction = (Direction)(((int)direction + 1) % 4);
+
+		switch (direction) {
+
+		case Direction.West:
+			CorridorX = xBasis;
+			CorridorY = Random.Range (yBasis + 1, yBasis + height - 1);
+			for (int i = 0; i < CorridorLength +2; i++) {
+				WallBoard.Add (new Vector2 (CorridorX - i, CorridorY + 1));
+				GroundBoard.Add (new Vector2 (CorridorX - i, CorridorY));
+				WallBoard.Add (new Vector2 (CorridorX - i, CorridorY - 1));
+			}
+			StartingXCoord = StartingXCoord - CorridorLength - width;
+			break;
+		case Direction.North:
+			CorridorX = Random.Range (xBasis + 1, xBasis + width - 1);
+			CorridorY = yBasis + height - 1;
+			for (int i = 0; i < CorridorLength + 2; i++) {
+				WallBoard.Add (new Vector2 (CorridorX + 1, CorridorY + i));
+				GroundBoard.Add (new Vector2 (CorridorX, CorridorY + i));
+				WallBoard.Add (new Vector2 (CorridorX - 1, CorridorY + i));
+			}
+			StartingYCoord = StartingYCoord + CorridorLength + height - 2;
+			break;
+		case Direction.East:
+			CorridorX = xBasis + width - 1;
+			CorridorY = Random.Range (yBasis + 1, yBasis + height - 1);
+			for (int i = 0; i < CorridorLength + 2; i++) {
+				WallBoard.Add (new Vector2 (CorridorX + i, CorridorY + 1));
+				GroundBoard.Add (new Vector2 (CorridorX + i, CorridorY));
+				WallBoard.Add (new Vector2 (CorridorX + i, CorridorY - 1));
+			}
+			StartingXCoord = StartingXCoord + CorridorLength + width;
+			break;
+		case Direction.South:
+			CorridorX = Random.Range (xBasis + 1, xBasis + width - 1);
+			CorridorY = yBasis;	
+			for (int i = 0; i < CorridorLength + 2; i++) {
+				WallBoard.Add (new Vector2 (CorridorX + 1, CorridorY - i));
+				GroundBoard.Add (new Vector2 (CorridorX, CorridorY - i));
+				WallBoard.Add (new Vector2 (CorridorX - 1, CorridorY - i));
+			}
+			StartingYCoord = StartingYCoord - CorridorLength - height + 2;
+			break;
+		}
+
+		incomingDirection = (Direction)(((int)direction + 2) % 4);
+
+
+	}
+
+	#endregion
+
+	#region reusable
+
+    void SpawnObjectAtCoordinates(GameObject[] objects, Vector2 pos)
     {
         GameObject ObjectToSpawn = objects[Random.Range(0, objects.Length)];
 
-        GameObject instance = Instantiate(ObjectToSpawn, new Vector2(x , y), Quaternion.identity) as GameObject;
+        GameObject instance = Instantiate(ObjectToSpawn, pos, Quaternion.identity) as GameObject;
 
         instance.transform.SetParent(DungHolder);
 
     }
 
-    void CreateCorridor(int xBasis, int yBasis, int width , int height )
-    {
-
-        direction = (Direction)Random.Range(0, 4);
-
-        if (direction == incomingDirection)
-        {
-            int directionIndex = (int)direction;
-            directionIndex = (directionIndex + 1) % 4;
-            direction = (Direction)directionIndex;
-        }
-
-        switch (direction)
-        {
-            case Direction.West:
-                CorridorX = xBasis;
-                CorridorY = Random.Range(yBasis + 1, yBasis + height - 1);
-                for (int i = 0; i < CorridorLength; i++)
-                {
-                    SpawnObjectAtCoordinates(WallTiles, CorridorX - i, CorridorY + 1);
-
-                    SpawnObjectAtCoordinates(FloorTiles, CorridorX - i, CorridorY);
-
-                    SpawnObjectAtCoordinates(WallTiles, CorridorX - i, CorridorY - 1);
-
-                }
-                SpawnObjectAtCoordinates(WallTiles, StartingXCoord, StartingYCoord + 1);
-                StartingXCoord = StartingXCoord - CorridorLength - width + 2;
-
-                break;
-                
-            case Direction.North:
-                CorridorX = Random.Range(xBasis + 1, xBasis + width - 1);
-                CorridorY = yBasis + height - 1;
-                for (int i = 0; i < CorridorLength; i++)
-                {
-                    SpawnObjectAtCoordinates(WallTiles, CorridorX + 1, CorridorY + i);
-
-                    SpawnObjectAtCoordinates(FloorTiles, CorridorX, CorridorY + i);
-
-                    SpawnObjectAtCoordinates(WallTiles, CorridorX - 1, CorridorY + i);
-
-                }
-                StartingYCoord = StartingYCoord + CorridorLength + height - 2;
-                break;
-
-            case Direction.South:
-                CorridorX = Random.Range(xBasis + 1, xBasis + width - 1) ;
-                CorridorY = yBasis;
-                for (int i = 0; i < CorridorLength; i++)
-                {
-                    SpawnObjectAtCoordinates(WallTiles, CorridorX + 1, CorridorY - i);
-
-                    SpawnObjectAtCoordinates(FloorTiles, CorridorX, CorridorY - i);
-
-                    SpawnObjectAtCoordinates(WallTiles, CorridorX - 1, CorridorY - i);
-
-                }
-                StartingYCoord = StartingYCoord - CorridorLength - height + 2;
-                break;
-
-            case Direction.East:
-                CorridorX = xBasis + width - 1 ;
-                CorridorY = Random.Range(yBasis + 1, yBasis + height - 1);
-                for (int i = 0; i < CorridorLength; i++)
-                {
-                    SpawnObjectAtCoordinates(WallTiles, CorridorX + i, CorridorY + 1);
-
-                    SpawnObjectAtCoordinates(FloorTiles, CorridorX + i, CorridorY);
-
-                    SpawnObjectAtCoordinates(WallTiles, CorridorX + i, CorridorY - 1);
-
-                }
-                StartingXCoord = StartingXCoord + CorridorLength + width - 2;
-                break;
-
-        }
-                incomingDirection = (Direction)(((int)direction + 2) % 4);
-        
-    }
-
-    void DungeonRoomCreate()
-    {
-
-
-        int xBasis = StartingXCoord;
-
-        int yBasis = StartingYCoord;
-
-        for (int x = 0; x < Width; x++)
-        {
-            for (int y = 0; y < Height; y++)
-            {
-                GameObject ObjectToSpawn = FloorTiles[Random.Range(0, FloorTiles.Length)];
-                if (x == 0 || x == Width - 1 || y == 0 || y == Height - 1)
-                    ObjectToSpawn = WallTiles[Random.Range(0, WallTiles.Length)];
-
-                GameObject instance = Instantiate(ObjectToSpawn, new Vector2(xBasis + x, yBasis + y), Quaternion.identity) as GameObject;
-
-                instance.transform.SetParent(DungHolder);
-            }
-        }
-
-        if (CurrentRoom == Rooms)
-            return;
-        CreateCorridor(xBasis, yBasis, Width, Height);
-
-    }
-
-    #region forgetIt
-    Vector2 RandomPosition()
-    {
-        int randomIndex = Random.Range(0, gridPositions.Count);
-        Vector2 randomPosition = gridPositions[randomIndex];
-        gridPositions.RemoveAt(randomIndex);
-        return randomPosition;
-    }
-
-   void SpawnRandomObject(GameObject[] tileArray, int min, int max)
-    {
-
-        int ObjectCount = Random.Range(min, max + 1);
-
-        for (int i = 0; i < ObjectCount; i++)
-        {
-            Vector2 randomPosition = RandomPosition();
-            GameObject tile = tileArray[Random.Range(0, tileArray.Length)];
-            Instantiate(tile, randomPosition, Quaternion.identity);
-
-        }
-    }
-#endregion
-
+    #endregion
+		
     public void SetupScene()
     {
         DungHolder = new GameObject("Dungeon").transform;
-        for (int i = 0; i < Rooms; i++)
-        {
-            CurrentRoom++;
-        DungeonRoomCreate();
+		for (int i = 0; i < GetRooms; i++) {
+			currentRoom++;
+			CreateVectorDungeon();
+		}
+		GrandCleaning ();
+		InitializeTiles();
         }
-
-
-        InitializeList();
     }
-
-}
