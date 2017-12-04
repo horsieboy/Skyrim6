27fe5acb1f9d06a932913a8e73a06d9b5ccdeaae
vvdashevskiy <vvdashevskiy@gmail.com>
2017-12-04 17:29:06 +0300

    /tts Memsha
    + Ability to close the game asap
    + Some other features

---
+++ HeartBroken Adventure/Assets/Scripts/Menu/AltInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AltInput : MonoBehaviour
+{
+    public EventSystem EventSystem;
+    public GameObject SelectedObject;
+
+    private bool ButtonSelected;
+    
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		if (Input.GetAxisRaw("Vertical") != 0 && ButtonSelected == false)
+        {
+            EventSystem.SetSelectedGameObject(SelectedObject);
+            ButtonSelected = true;
+        }
+	}
+
+    private void OnDisable()
+    {
+        ButtonSelected = false;
+    }
+}
